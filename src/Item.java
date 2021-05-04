9d20849c45fa095ff16da1fe6a1a8e2cb989823a
Pieter Dirix <pieter-dirix@hotmail.com>
2021-05-04 11:33:50 +0200

    feat(item): added parameters, updateItem method

--- src/Item.java
+++ src/Item.java
@@ -1,2 +1,46 @@
+import java.lang.reflect.Constructor;
+
 public class Item {
+    final static int IN_STOCK = 0;
+    final static int OUT_OF_STOCK = 1;
+
+    private int state = IN_STOCK;
+    private String name;
+    private String description;
+    private float price;
+    private int stock;
+
+    public Item(String name, String description, float price, int stock) {
+        this.name = name;
+        this.description = description;
+        this.price = price;
+        this.stock = stock;
+        if (stock == 0 ) {
+            this.state = OUT_OF_STOCK;
+        } else {
+            this.state = IN_STOCK;
+        }
+    }
+
+    public void setPrice(float price) {
+        this.price = price;
+    }
+
+    public void setState(int state) {
+        this.state = state;
+    }
+
+    public void setStock(int stock) {
+        this.stock = stock;
+    }
+
+    public void updateItem(Item item) {
+        this.setPrice(item.price);
+
+        if ( item.stock == 0 ) {
+            this.setState(OUT_OF_STOCK);
+        }
+
+        this.setStock(item.stock);
+    }
 }
